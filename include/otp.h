3d7d66560d7cfcd887ea654c33dfae8277b950bc
Jackson Ming Hu <huming2207@gmail.com>
2019-05-04 22:41:27 +1000

    otp: make C++ lovers happier

--- include/otp.h
+++ include/otp.h
@@ -3,6 +3,14 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 uint32_t hotp_generate(uint8_t *key, size_t key_len, uint64_t interval, size_t digits);
 uint32_t totp_hash_token(uint8_t *key, size_t key_len, uint64_t time, size_t digits);
 uint32_t totp_generate(uint8_t *key, size_t key_len);
+
+#ifdef __cplusplus
+}
+#endif
